61ea0e0631b5d0b0ba94ad7a1a4069074b34e5e0
AkitoTsukahara <a-tsukahara@macloud.jp>
2021-11-20 23:45:49 +0900

    lesson4 type

--- lesson04/type.go
+++ lesson04/type.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
+	var i int = 1
+	fl64 := float64(i)
+	fmt.Println(fl64)
+
+	var s string = "100"
+	is, _ := strconv.Atoi(s)
+	fmt.Println(is)
+
+	var ii int = 200
+	ss := strconv.Itoa(ii)
+	fmt.Println(ss)
 
 }
